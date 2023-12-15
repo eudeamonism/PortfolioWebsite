ed6b1668bb93f26f6815fa7d81b71120bf15c6d1
E V <evanvargas@gmail.com>
2023-12-15 15:38:00 -0600

    Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import { inject } from "@vercel/analytics";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Skills from "./screens/Skills";
 import NavBar from "./screens/NavBar";
 import Projects from "./screens/Projects";
-import Contact from './screens/Contact'
+import Contact from "./screens/Contact";
 function App() {
+  inject();
   return (
     <>
       <NavBar />
